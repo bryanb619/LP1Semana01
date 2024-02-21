c01155a5122e64c1648c9945f13b301dd6fa8881
Steven Hall <73695945+bryanb619@users.noreply.github.com>
2024-02-21 20:37:51 +0000

    Added Project VariosTypes
    Added program tha prints integral types in console

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        /// <summary>
+        ///  Displays in console various integral types
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            // integer
+            int i = 10; 
+            // unassigned integer
+            uint j = 5U;
+
+            // long
+            long k = 8L; 
+            // unassigned long
+            ulong n = 3UL; 
+
+            // sbyte
+            sbyte sb = 110; 
+
+            // byte
+            byte b = 234; 
+
+            // char
+            char c = 'a';
+
+            // prints 
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+            Console.WriteLine(n);
+            Console.WriteLine(sb);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+
+        }
+    }
+}
