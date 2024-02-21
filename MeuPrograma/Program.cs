7d919615fcc0062f2476e7b9c8b213c4988c40b8
Steven Hall <73695945+bryanb619@users.noreply.github.com>
2024-02-21 19:56:40 +0000

    Added XML documentation to class Program & Main

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,16 +2,24 @@
 
 namespace MeuPrograma
 {
+
+    /// <summary>
+    ///  Basic program that prints messages to the user 
+    /// </summary>
     class Program
     {
-        static void Main(string[] args)
+
+        /// <summary> 
+        ///  Outputs two messages to the console
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
         {
             // hello world instruction
             Console.WriteLine("Hello World!");
             // Bye world instruction
             Console.WriteLine("Bye world”");
 
-            
         }
     }
 }
