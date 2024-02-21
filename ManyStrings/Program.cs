a5bfff524059049416686f7cf2fb62ad054f1281
Steven Hall <73695945+bryanb619@users.noreply.github.com>
2024-02-21 21:15:30 +0000

    Added strings sum & interpolations

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -17,6 +17,27 @@ namespace ManyStrings
 
             Console.WriteLine(someString);
             Console.WriteLine(path);
+
+            // 5
+
+            int x = 5, y = 3; 
+
+            string a = "a" + 2; 
+
+            string b = "abc" + x; 
+
+            Console.WriteLine(a); 
+            Console.WriteLine(b); 
+
+            // interpolações
+            Console.WriteLine($"O valor de e' {x}");
+            Console.WriteLine($"{x} + {y} = {x+y}");
+
+
+
+
+
+
         }
     }
 }
--- ManyStrings/obj/Debug/net6.0/ManyStrings.AssemblyInfo.cs
+++ ManyStrings/obj/Debug/net6.0/ManyStrings.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManyStrings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd3290882ed8a00b0ac23460a074995dcaf382b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9ee2d52dcf5bbf883c4c4fb30ec25b064c1633")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManyStrings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManyStrings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
