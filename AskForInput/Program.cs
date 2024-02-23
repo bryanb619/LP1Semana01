2c74d57b206490ff4c2f98d73eaaa87267c7e514
Steven Hall <73695945+bryanb619@users.noreply.github.com>
2024-02-23 14:12:33 +0000

    Added conversion

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -6,7 +6,18 @@ namespace AskForInput
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // Request user input for an integer
+            Console.WriteLine("Inseri um número inteiro");
+
+            // Read the input and convert to int
+            int intInput = int.Parse(Console.ReadLine());
+
+            // Request user input for a real number
+            Console.WriteLine("Inseri um número real");
+
+            // Read the input and convert to float
+            float realInput = float.Parse(Console.ReadLine());
         }
     }
 }
